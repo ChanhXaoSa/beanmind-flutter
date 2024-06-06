05cbe3cc8436cb06eb897beb8424b12dce015803
chanhday <trieudoublenguyen@gmail.com>
2024-06-06 17:57:03 +0700

    update layout lan 2

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,6 +1,9 @@
+import 'package:beanmind_flutter/hexcolor.dart';
 import 'package:flutter/material.dart';
 
 class BuildLayoutDemo extends StatefulWidget {
+  const BuildLayoutDemo({super.key});
+
   @override
   _BuildLayoutDemoState createState() => _BuildLayoutDemoState();
 }
@@ -9,76 +12,180 @@ class _BuildLayoutDemoState extends State<BuildLayoutDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF3E413C),
+        title: Row(
+          children: [
+            SizedBox(width: 10),
+            Text('KooBits', style: TextStyle(color: Colors.white),),
+            Spacer(),
+            IconButton(icon: Icon(Icons.notifications ,color: Colors.white), onPressed: () {}),
+            IconButton(icon: Icon(Icons.settings ,color: Colors.white), onPressed: () {}),
+          ],
+        ),
+        actions: [
+          // Add any additional actions here
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('User'),
+              accountEmail: Text('Level 1 / 1k'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage('https://via.placeholder.com/150'), // Profile picture
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xFF605BD9),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.flag),
+              title: Text('KooChallenge'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.school),
+              title: Text('KooTraining'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.class_),
+              title: Text('KooClass'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.gamepad),
+              title: Text('KooFun'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Row(
         children: [
           Container(
-            padding: EdgeInsets.all(16),
-            color: Colors.grey[800],
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            width: MediaQuery.of(context).size.width * 0.12,
+            color: Color(0xFF605BD9),
+            child: Column(
               children: [
-                // Image.network(
-                //   'https://i.pinimg.com/236x/ab/3d/e2/ab3de2f5cc08f507f728f39c66e596b8.jpg',
-                //   height: 50,
-                // ),
-                Text(
-                  'BeanMind',
-                  style: TextStyle(fontSize: 24, color: Colors.white),
+                SizedBox(height: 20,),
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: NetworkImage('https://via.placeholder.com/150'), // Profile picture
                 ),
-                Row(
-                  children: [
-                    Text(
-                      'Username',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                    SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                      },
-                      child: Text('Logout'),
-                    ),
-                  ],
+                SizedBox(height: 20,),
+                Text('User', style: TextStyle(fontSize: 20, color: Colors.white),),
+                SizedBox(height: 20,),
+                ListTile(
+                  leading: Icon(Icons.flag, color: Colors.white,),
+                  title: Text('Challenge', style: TextStyle(color: Colors.white),),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.school, color: Colors.white,),
+                  title: Text('Training', style: TextStyle(color: Colors.white),),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.class_, color: Colors.white,),
+                  title: Text('Class', style: TextStyle(color: Colors.white),),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.gamepad, color: Colors.white,),
+                  title: Text('Fun', style: TextStyle(color: Colors.white),),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.supervised_user_circle, color: Colors.white),
+                  title: Text('Friends', style: TextStyle(color: Colors.white),),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                  },
                 ),
               ],
             ),
           ),
-          // Main content
           Expanded(
-            child: Row(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.15,
-                  color: Colors.green[600],
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ListTile(
-                        title: Text('Learning'),
-                        textColor: Colors.white,
-                        onTap: () {
-                        },
-                      ),
-                      ListTile(
-                        title: Text('Minigame'),
-                        textColor: Colors.white,
-                        onTap: () {
-                        },
-                      ),
-                      ListTile(
-                        title: Text('Daily Quest'),
-                        textColor: Colors.white,
-                        onTap: () {
-                        },
-                      ),
-                    ],
-                  ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        buildCard('Daily Challenge', 'Sunday Mini', 'Start', 'Every Mon - Sat', Colors.orange, Colors.white),
+                        buildCard('Peer Challenge', '', '', '', Colors.deepOrange, Colors.white),
+                        buildCard('Super Hero Challenge', '', '', '', Colors.blue, Colors.white),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    // Additional content can be added here
+                  ],
                 ),
-                // Right side content
-                Expanded(
-                  child: Text('Hello'),
-                ),
-              ],
+              ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+  Widget buildCard(String title, String subtitle, String buttonText, String description, Color color, Color textColor) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: textColor),
+          ),
+          Text(
+            subtitle,
+            style: TextStyle(fontSize: 16, color: textColor),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(buttonText),
+          ),
+          Text(
+            description,
+            style: TextStyle(fontSize: 12, color: textColor),
           ),
         ],
       ),
