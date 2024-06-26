1052e8260837b205eab34cf19b88a84080a66b07
Type 59 - 390 <108561127+LETUANKIET29@users.noreply.github.com>
2024-06-27 00:40:43 +0700

    Update course_details_screen.dart

--- lib/screens/course/course_details_screen.dart
+++ lib/screens/course/course_details_screen.dart
@@ -32,8 +32,7 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
   final double rating = 4.5;
   final int numRatings = 100;
   final String duration = 'Thời lượng khoá học';
-  final String courseDescription =
-      'Mô tả chi tiết về khoá học';
+  final String courseDescription = 'Mô tả chi tiết về khoá học';
 
   @override
   Widget build(BuildContext context) {
@@ -43,14 +42,14 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(5),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // Image Section
               ClipRRect(
                 borderRadius: BorderRadius.circular(15),
-                child: Image.network(
+                child: Image.asset(
                   courseImageUrl,
                   height: 200,
                   width: double.infinity,
@@ -58,77 +57,161 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
                 ),
               ),
               const SizedBox(height: 16),
-
-              // Course Title
-              Text(
-                courseTitle,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8),
-
-              // Instructor Name
-              Text(
-                'Người tạo khoá học: $instructor',
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey,
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Rating and Number of Ratings
+              // Description and Instructor Info Row
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.star, color: Colors.orange, size: 20),
-                  const SizedBox(width: 4),
-                  Text(
-                    '$rating ($numRatings đánh giá)',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
+                  // Description Column
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Course Title
+                          Text(
+                            courseTitle,
+                            style: const TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Mô tả khoá học',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            courseDescription,
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  // Instructor Info Column
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 600,
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Người tạo khoá học: $instructor',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+
+                          // Rating and Number of Ratings
+                          Row(
+                            children: [
+                              const Icon(Icons.star,
+                                  color: Colors.orange, size: 20),
+                              const SizedBox(width: 4),
+                              Text(
+                                '$rating ($numRatings đánh giá)',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+
+                          Row(
+                            children: [
+                              const Icon(Icons.account_circle,
+                                  color: Colors.orange, size: 20),
+                              const SizedBox(width: 4),
+                              Text(
+                                'số lượng người đã đăng kí $numRatings',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+
+                          // Duration
+                          Row(
+                            children: [
+                              const Icon(Icons.access_time,
+                                  color: Colors.grey, size: 20),
+                              const SizedBox(width: 4),
+                              Text(
+                                duration,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+
+                          // đẩy nút xuống dưới
+                          const Spacer(),
+                          Row(
+                            children: [
+                              Expanded(                             
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    // Handle course enrollment or start action
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    textStyle: const TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,   
+                                      color: Colors.white,                             
+                                    ),
+                                    minimumSize: const Size(double.infinity, 60),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(1),
+                                    ),
+                                    backgroundColor: Colors.blue,
+                                    foregroundColor: Colors.white,
+                                  ),
+                                  child: const Text('Đăng ký khoá học'),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 8),
-
-              // Duration
-              Row(
-                children: [
-                  Icon(Icons.access_time, color: Colors.grey, size: 20),
-                  const SizedBox(width: 4),
-                  Text(
-                    duration,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
               const SizedBox(height: 16),
-
-              // Course Description
-              Text(
-                courseDescription,
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Start/Enroll Button
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Handle course enrollment or start action
-                  },
-                  child: const Text('Đăng ký khoá học'),
-                ),
-              ),
+              // Center(
+              //   child: ElevatedButton(
+              //     onPressed: () {
+              //       // Handle course enrollment or start action
+              //     },
+              //     child: const Text('Đăng ký khoá học'),
+              //   ),
+              // ),
             ],
           ),
         ),
