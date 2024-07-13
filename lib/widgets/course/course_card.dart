b0f5234cbea009ca712189257fb27698e8e22cea
chanhday <trieudoublenguyen@gmail.com>
2024-07-13 23:47:55 +0700

    Update course_card.dart

--- lib/widgets/course/course_card.dart
+++ lib/widgets/course/course_card.dart
@@ -1,14 +1,9 @@
-import 'package:easy_separator/easy_separator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:beanmind_flutter/configs/configs.dart';
 import 'package:beanmind_flutter/controllers/controllers.dart';
 import 'package:beanmind_flutter/controllers/quiz_paper/quiz_papers_controller.dart';
-import 'package:beanmind_flutter/models/quiz_paper_model.dart';
-import 'package:beanmind_flutter/screens/screens.dart';
-import 'package:beanmind_flutter/widgets/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CourseCard extends GetView<QuizPaperController> {
   const CourseCard({Key? key}) : super(key: key);
