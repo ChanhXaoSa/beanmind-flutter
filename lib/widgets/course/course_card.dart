7a1985f7b705b99a2f32d7984decf7484203137e
chanhday <trieudoublenguyen@gmail.com>
2024-07-13 23:56:17 +0700

    update ignore private variable warning and course card

--- lib/widgets/course/course_card.dart
+++ lib/widgets/course/course_card.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:beanmind_flutter/configs/configs.dart';
 import 'package:beanmind_flutter/controllers/controllers.dart';
 import 'package:beanmind_flutter/controllers/quiz_paper/quiz_papers_controller.dart';
 
 class CourseCard extends GetView<QuizPaperController> {
-  const CourseCard({Key? key}) : super(key: key);
+  const CourseCard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,30 +26,30 @@ class CourseCard extends GetView<QuizPaperController> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 4),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 4),
                   child: SizedBox(
                     width: 285,
                     child: Container(
-                      padding: EdgeInsets.fromLTRB(10.5, 8, 15, 15),
+                      padding: const EdgeInsets.fromLTRB(10.5, 8, 15, 15),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Container(
-                            margin: EdgeInsets.fromLTRB(0, 0, 0, 120),
+                            margin: const EdgeInsets.fromLTRB(0, 0, 0, 120),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Container(
-                                  margin: EdgeInsets.fromLTRB(0, 0, 8.5, 0),
+                                  margin: const EdgeInsets.fromLTRB(0, 0, 8.5, 0),
                                   child: Container(
                                     decoration: BoxDecoration(
-                                      color: Color(0xFF3DCBB1),
+                                      color: const Color(0xFF3DCBB1),
                                       borderRadius: BorderRadius.circular(23),
                                     ),
                                     child: Container(
-                                      padding: EdgeInsets.fromLTRB(4, 4, 8.1, 4),
+                                      padding: const EdgeInsets.fromLTRB(4, 4, 8.1, 4),
                                       child:
                                       Text(
                                         'Best Seller',
@@ -58,7 +57,7 @@ class CourseCard extends GetView<QuizPaperController> {
                                           fontWeight: FontWeight.w700,
                                           fontSize: 8,
                                           height: 1.3,
-                                          color: Color(0xFFFFFFFF),
+                                          color: const Color(0xFFFFFFFF),
                                         ),
                                       ),
                                     ),
@@ -66,11 +65,11 @@ class CourseCard extends GetView<QuizPaperController> {
                                 ),
                                 Container(
                                   decoration: BoxDecoration(
-                                    color: Color(0xFFA04AE3),
+                                    color: const Color(0xFFA04AE3),
                                     borderRadius: BorderRadius.circular(23),
                                   ),
                                   child: Container(
-                                    padding: EdgeInsets.fromLTRB(4, 4, 8.5, 4),
+                                    padding: const EdgeInsets.fromLTRB(4, 4, 8.5, 4),
                                     child:
                                     Text(
                                       '20% OFF',
@@ -78,7 +77,7 @@ class CourseCard extends GetView<QuizPaperController> {
                                         fontWeight: FontWeight.w700,
                                         fontSize: 8,
                                         height: 1.3,
-                                        color: Color(0xFFFFFFFF),
+                                        color: const Color(0xFFFFFFFF),
                                       ),
                                     ),
                                   ),
@@ -87,11 +86,11 @@ class CourseCard extends GetView<QuizPaperController> {
                             ),
                           ),
                           Container(
-                            margin: EdgeInsets.fromLTRB(0, 126, 0, 0),
+                            margin: const EdgeInsets.fromLTRB(0, 126, 0, 0),
                             width: 12,
                             height: 12,
                             child:
-                            SizedBox(
+                            const SizedBox(
                               width: 12,
                               height: 12,
                               // child: SvgPicture.network(
@@ -105,7 +104,7 @@ class CourseCard extends GetView<QuizPaperController> {
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 4),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 4),
                   child: Align(
                     alignment: Alignment.topLeft,
                     child: Text(
@@ -114,13 +113,13 @@ class CourseCard extends GetView<QuizPaperController> {
                         fontWeight: FontWeight.w700,
                         fontSize: 16,
                         height: 1.3,
-                        color: Color(0xFF000000),
+                        color: const Color(0xFF000000),
                       ),
                     ),
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(3, 0, 3, 4),
+                  margin: const EdgeInsets.fromLTRB(3, 0, 3, 4),
                   child: Align(
                     alignment: Alignment.topLeft,
                     child: Row(
@@ -128,11 +127,11 @@ class CourseCard extends GetView<QuizPaperController> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(
-                          margin: EdgeInsets.fromLTRB(0, 3, 7, 3),
+                          margin: const EdgeInsets.fromLTRB(0, 3, 7, 3),
                           width: 12,
                           height: 12,
                           child:
-                          SizedBox(
+                          const SizedBox(
                             width: 12,
                             height: 12,
                             // child: SvgPicture.network(
@@ -146,7 +145,7 @@ class CourseCard extends GetView<QuizPaperController> {
                             fontWeight: FontWeight.w400,
                             fontSize: 14,
                             height: 1.3,
-                            color: Color(0xFF3DCBB1),
+                            color: const Color(0xFF3DCBB1),
                           ),
                         ),
                       ],
@@ -154,19 +153,19 @@ class CourseCard extends GetView<QuizPaperController> {
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 9.4, 22),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 9.4, 22),
                   child: Text(
                     'More than 8yr Experience as Illustrator. Learn how to becoming professional Illustrator Now...',
                     style: kDetailsTS.copyWith(
                       fontWeight: FontWeight.w400,
                       fontSize: 14,
                       height: 1.3,
-                      color: Color(0xE51B1B1B),
+                      color: const Color(0xE51B1B1B),
                     ),
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(2.3, 0, 2.3, 4),
+                  margin: const EdgeInsets.fromLTRB(2.3, 0, 2.3, 4),
                   child: Align(
                     alignment: Alignment.topLeft,
                     child: Row(
@@ -174,7 +173,7 @@ class CourseCard extends GetView<QuizPaperController> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(
-                          margin: EdgeInsets.fromLTRB(0, 3.5, 5.3, 3.5),
+                          margin: const EdgeInsets.fromLTRB(0, 3.5, 5.3, 3.5),
                           child: SizedBox(
                             width: 83.4,
                             child: Row(
@@ -182,11 +181,11 @@ class CourseCard extends GetView<QuizPaperController> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Container(
-                                  margin: EdgeInsets.fromLTRB(0, 0, 6.6, 0),
+                                  margin: const EdgeInsets.fromLTRB(0, 0, 6.6, 0),
                                   width: 11.4,
                                   height: 10.9,
                                   child:
-                                  SizedBox(
+                                  const SizedBox(
                                     width: 11.4,
                                     height: 10.9,
                                     // child: SvgPicture.network(
@@ -195,11 +194,11 @@ class CourseCard extends GetView<QuizPaperController> {
                                   ),
                                 ),
                                 Container(
-                                  margin: EdgeInsets.fromLTRB(0, 0, 6.6, 0),
+                                  margin: const EdgeInsets.fromLTRB(0, 0, 6.6, 0),
                                   width: 11.4,
                                   height: 10.9,
                                   child:
-                                  SizedBox(
+                                  const SizedBox(
                                     width: 11.4,
                                     height: 10.9,
                                     // child: SvgPicture.network(
@@ -208,11 +207,11 @@ class CourseCard extends GetView<QuizPaperController> {
                                   ),
                                 ),
                                 Container(
-                                  margin: EdgeInsets.fromLTRB(0, 0, 6.6, 0),
+                                  margin: const EdgeInsets.fromLTRB(0, 0, 6.6, 0),
                                   width: 11.4,
                                   height: 10.9,
                                   child:
-                                  SizedBox(
+                                  const SizedBox(
                                     width: 11.4,
                                     height: 10.9,
                                     // child: SvgPicture.network(
@@ -221,11 +220,11 @@ class CourseCard extends GetView<QuizPaperController> {
                                   ),
                                 ),
                                 Container(
-                                  margin: EdgeInsets.fromLTRB(0, 0, 6.6, 0),
+                                  margin: const EdgeInsets.fromLTRB(0, 0, 6.6, 0),
                                   width: 11.4,
                                   height: 10.9,
                                   child:
-                                  SizedBox(
+                                  const SizedBox(
                                     width: 11.4,
                                     height: 10.9,
                                     // child: SvgPicture.network(
@@ -233,7 +232,7 @@ class CourseCard extends GetView<QuizPaperController> {
                                     // ),
                                   ),
                                 ),
-                                Container(
+                                const SizedBox(
                                   width: 11.4,
                                   height: 10.9,
                                   child:
@@ -255,7 +254,7 @@ class CourseCard extends GetView<QuizPaperController> {
                             fontWeight: FontWeight.w400,
                             fontSize: 14,
                             height: 1.3,
-                            color: Color(0x991B1B1B),
+                            color: const Color(0x991B1B1B),
                           ),
                         ),
                       ],
@@ -269,19 +268,19 @@ class CourseCard extends GetView<QuizPaperController> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 6.7, 0),
+                        margin: const EdgeInsets.fromLTRB(0, 0, 6.7, 0),
                         child: Text(
                           '\$24.92',
                           style: kDetailsTS.copyWith(
                             fontWeight: FontWeight.w700,
                             fontSize: 20,
                             height: 1.3,
-                            color: Color(0xE51B1B1B),
+                            color: const Color(0xE51B1B1B),
                           ),
                         ),
                       ),
                       Container(
-                        margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
+                        margin: const EdgeInsets.fromLTRB(0, 1, 0, 1),
                         child: Text(
                           '\$32.90',
                           style: kDetailsTS.copyWith(
@@ -289,8 +288,8 @@ class CourseCard extends GetView<QuizPaperController> {
                             fontSize: 16,
                             decoration: TextDecoration.lineThrough,
                             height: 1.5,
-                            color: Color(0x991B1B1B),
-                            decorationColor: Color(0x991B1B1B),
+                            color: const Color(0x991B1B1B),
+                            decorationColor: const Color(0x991B1B1B),
                           ),
                         ),
                       ),
