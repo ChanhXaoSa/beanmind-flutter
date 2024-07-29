a1fead04a39c7152951cfde7de63844018f49e84
Xe tăng tông cổng <108561127+LETUANKIET29@users.noreply.github.com>
2024-07-29 18:03:35 +0700

    save game result, not yet fix

--- lib/widgets/game/class/save_game_result.dart
+++ lib/widgets/game/class/save_game_result.dart
@@ -1,26 +1,55 @@
+import 'dart:convert';
+
 import 'package:beanmind_flutter/controllers/auth_controller.dart';
+import 'package:beanmind_flutter/controllers/controllers.dart';
 import 'package:beanmind_flutter/firebase/references.dart';
+import 'package:beanmind_flutter/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 Future<void> saveGameResults(String gameId, int points, int userPoint,
     int userProgress, int timeRecord) async {
-  var batch = fi.batch();
-  User? _user = Get.find<AuthController>().getUser();
-  if (_user == null) return;
-  batch.set(userFR.doc(_user.email).collection('myrecent_games').doc(gameId), {
-    "points": points,
-    "correct_count": '$userPoint/$userProgress',
-    "paper_id": gameId,
-    "time": timeRecord
+  final MyDrawerController controller = Get.find();
+  if (controller.user.value!.first.data.id == null) return;
+
+  // Calculate duration from timeRecord
+  final durationInSeconds = timeRecord;
+  final hours = durationInSeconds ~/ 3600;
+  final minutes = (durationInSeconds % 3600) ~/ 60;
+
+  final apiUrl =
+      "https://smart-platypus-hardly.ngrok-free.app/api/v1/game-histories";
+  final body = jsonEncode({
+    "gameId": gameId,
+    "applicationUserId": controller.user.value!.first.data.id,
+    "point": points,
+    "duration": {
+      "hour": hours,
+      "minute": minutes,
+    },
   });
-  batch.set(
-      leaderBoardFR.doc(gameId).collection('game_scores').doc(_user.email), {
-    "points": points,
-    "correct_count": '$userPoint/$userProgress',
-    "paper_id": gameId,
-    "user_id": _user.email,
-    "time": timeRecord
-  });
-  await batch.commit();
+
+  try {
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=utf-8',
+        'ngrok-skip-browser-warning': 'true',
+      },
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      // Handle successful response
+      print("Game results saved successfully.");
+    } else {
+      // Handle error response
+      throw Exception(
+          'Failed to save game results. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    // Handle exception
+    print("Error saving game results: $e");
+  }
 }
