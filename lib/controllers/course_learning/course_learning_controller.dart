f374521955535b982c7c8691cdd503e641fb6781
chanhday <trieudoublenguyen@gmail.com>
2024-08-03 14:31:21 +0700

    Update course_learning_controller.dart

--- lib/controllers/course_learning/course_learning_controller.dart
+++ lib/controllers/course_learning/course_learning_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:beanmind_flutter/models/chapter_model.dart';
 import 'package:beanmind_flutter/models/course_detail_model.dart';
 import 'package:beanmind_flutter/models/topic_model.dart';
+import 'package:beanmind_flutter/utils/api_endpoint.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,6 +15,7 @@ class CourseLearningController extends GetxController {
   var chapterList = <ChapterItem>[].obs;
   var topicModel = Rxn<TopicModel>();
   var topicListModel = <TopicItem>[].obs;
+  var expandedChapters = <String, bool>{}.obs;
 
   var selectedContent = 'Select a topic to see the content'.obs;
   var sections = <Section>[
@@ -29,6 +33,95 @@ class CourseLearningController extends GetxController {
   void onInit() {
     courseId = Get.parameters['id']!;
     super.onInit();
+  }
+
+  Future<void> fetchTopic(String chapterId) async {
+    try {
+      final topicResponse = await http.get(
+          Uri.parse('${newBaseApiUrl}/topics?ChapterId=${chapterId}'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=utf-8',
+            'ngrok-skip-browser-warning': 'true',
+          }
+      );
+      if (topicResponse.statusCode == 200) {
+        final topicModelBase = TopicModel.fromJson(json.decode(topicResponse.body));
+        topicModel.value = topicModelBase;
+        if(topicModelBase.data?.items != null) {
+          topicListModel.addAll(topicModelBase.data!.items!);
+        }
+        for (var topic in topicListModel) {
+          print('Topic: ${topic.title}, ChapterId: ${topic.chapterId}');
+        }
+      } else {
+        throw Exception('Failed to fetch topic');
+      }
+    } catch (e) {
+      print('Error: $e');
+      throw e;
+    }
+  }
+
+  Future<void> fetchChapter() async {
+    try {
+      final chapterResponse = await http.get(
+          Uri.parse('${newBaseApiUrl}/chapters?CourseId=${courseId}'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=utf-8',
+            'ngrok-skip-browser-warning': 'true',
+          }
+      );
+      if (chapterResponse.statusCode == 200) {
+        final chapterModelBase = ChapterModel.fromJson(json.decode(chapterResponse.body));
+        chapterModel.value = chapterModelBase;
+        if(chapterModelBase.data?.items != null)  {
+          chapterList.assignAll(chapterModelBase.data!.items!);
+          for (var chapter in chapterList) {
+            fetchTopic(chapter.id!);
+            expandedChapters[chapter.id!] = false;
+          }
+        }
+        // if(chapterModelBase.data?.items != null)  {
+        //   for (var chapter in chapterModelBase.data!.items!) {
+        //     fetchTopic(chapter.id!);
+        //   }
+        // }
+        print('${chapterList.toString()}');
+      } else {
+        throw Exception('Failed to fetch chapter');
+      }
+    } catch (e) {
+      print('Error: $e');
+      throw e;
+    }
+  }
+
+  Future<void> fetchCourseDetail() async {
+    try {
+      final courseResponse = await http.get(
+          Uri.parse('${newBaseApiUrl}/courses/${courseId}'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=utf-8',
+            'ngrok-skip-browser-warning': 'true',
+          }
+      );
+      if (courseResponse.statusCode == 200) {
+        final courseDetailModelBase = CourseDetailModel.fromJson(json.decode(courseResponse.body));
+        courseDetailModel.value = courseDetailModelBase;
+        courseDetailData.value = courseDetailModelBase.data;
+        print(courseDetailModel.value.toString());
+      } else {
+        throw Exception('Failed to fetch course');
+      }
+    } catch (e) {
+      print('Error: $e');
+      throw e;
+    }
+  }
+
+  void toggleChapterExpansion(String chapterId) {
+    expandedChapters[chapterId] = !(expandedChapters[chapterId] ?? false);
+    update();
   }
 
   void selectContent(String content) {
