35c89e30c720cff1b0e45cdb10e3575b05834d7f
chanhday <trieudoublenguyen@gmail.com>
2024-08-05 04:18:11 +0700

    add new banner style

--- lib/widgets/home/custom_banner.dart
+++ lib/widgets/home/custom_banner.dart
@@ -1,3 +1,5 @@
+import 'package:beanmind_flutter/widgets/common/circle_button.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class CustomBanner extends StatefulWidget {
@@ -6,123 +8,96 @@ class CustomBanner extends StatefulWidget {
 }
 
 class _CustomBannerState extends State<CustomBanner> {
-  final PageController _controller = PageController();
-
-  final List<String> _images = [
-    'images/background/background_farm.png',
-    'images/background/background_ocean.png',
-    'images/background/background_shopping_game.png',
-  ];
-
-  int _currentIndex = 0;
-
-  void _nextPage() {
-    if (_currentIndex < _images.length - 1) {
-      _controller.nextPage(
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeIn,
-      );
-      setState(() {
-        _currentIndex++;
-      });
-    }
-  }
-
-  void _previousPage() {
-    if (_currentIndex > 0) {
-      _controller.previousPage(
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeIn,
-      );
-      setState(() {
-        _currentIndex--;
-      });
-    }
-  }
+  int _current = 0;
+  CarouselController _controller = CarouselController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300.0,
-      child: Stack(
-        children: [
-          PageView.builder(
-            controller: _controller,
-            itemCount: _images.length,
-            onPageChanged: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            itemBuilder: (context, index) {
-              return Image.asset(
-                _images[index],
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: 300.0,
-              );
-            },
+    return Column(
+      children: [
+        Container(
+          child: CarouselSlider(
+            items: imageSliders,
+            carouselController: _controller,
+            options: CarouselOptions(
+                autoPlay: true,
+                enlargeCenterPage: true,
+                aspectRatio: 2.0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                }),
           ),
-          Positioned(
-            left: 16.0,
-            top: 0,
-            bottom: 0,
-            child: Center(
-              child: IconButton(
-                icon: Icon(Icons.arrow_back, color: Colors.white),
-                onPressed: _previousPage,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: imgList.asMap().entries.map((entry) {
+            return GestureDetector(
+              onTap: () => _controller.animateToPage(entry.key),
+              child: Container(
+                width: 12.0,
+                height: 12.0,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.dark
+                        ? Colors.white
+                        : Colors.black)
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
               ),
-            ),
-          ),
-          Positioned(
-            right: 16.0,
-            top: 0,
-            bottom: 0,
-            child: Center(
-              child: IconButton(
-                icon: Icon(Icons.arrow_forward, color: Colors.white),
-                onPressed: _nextPage,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 16.0,
-            left: 16.0,
-            right: 16.0,
-            child: Container(
-              color: Colors.black.withOpacity(0.5),
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Make yourself two',
-                    style: TextStyle(color: Colors.white, fontSize: 24.0),
-                  ),
-                  Text(
-                    'Comfort & Professional',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 8.0),
-                  Text(
-                    'Comprehensive educational experiences that develop and enhance skill sets that can be applied to diverse job profiles.',
-                    style: TextStyle(color: Colors.white, fontSize: 16.0),
-                  ),
-                  SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Find your desired Course'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 }
+
+final List<String> imgList = [
+  'images/background/background_farm.png',
+  'images/background/background_ocean.png',
+  'images/background/background_shopping_game.png',
+  'images/background/background_store.png',
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+  child: Container(
+    margin: EdgeInsets.all(5.0),
+    child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Stack(
+          children: <Widget>[
+            Image.asset(item, fit: BoxFit.cover, width: 1000.0),
+            // Positioned(
+            //   bottom: 0.0,
+            //   left: 0.0,
+            //   right: 0.0,
+            //   child: Container(
+            //     decoration: BoxDecoration(
+            //       gradient: LinearGradient(
+            //         colors: [
+            //           Color.fromARGB(200, 0, 0, 0),
+            //           Color.fromARGB(0, 0, 0, 0)
+            //         ],
+            //         begin: Alignment.bottomCenter,
+            //         end: Alignment.topCenter,
+            //       ),
+            //     ),
+            //     padding: EdgeInsets.symmetric(
+            //         vertical: 10.0, horizontal: 20.0),
+            //     child: Text(
+            //       'No. ${imgList.indexOf(item)} image',
+            //       style: TextStyle(
+            //         color: Colors.white,
+            //         fontSize: 20.0,
+            //         fontWeight: FontWeight.bold,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+          ],
+        )),
+  ),
+)).toList();
