8b76dfc667b3aa190e6b4362daf9a7429b188109
chanhday <trieudoublenguyen@gmail.com>
2024-08-05 16:49:54 +0700

    Update custom_banner.dart

--- lib/widgets/home/custom_banner.dart
+++ lib/widgets/home/custom_banner.dart
@@ -1,4 +1,3 @@
-
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -63,39 +62,39 @@ final List<String> imgList = [
 
 final List<Widget> imageSliders = imgList
     .map((item) => Container(
-      margin: const EdgeInsets.all(5.0),
-      child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-          child: Stack(
-            children: <Widget>[
-              Image.asset(item, fit: BoxFit.cover, width: 1000.0),
-              // Positioned(
-              //   bottom: 0.0,
-              //   left: 0.0,
-              //   right: 0.0,
-              //   child: Container(
-              //     decoration: BoxDecoration(
-              //       gradient: LinearGradient(
-              //         colors: [
-              //           Color.fromARGB(200, 0, 0, 0),
-              //           Color.fromARGB(0, 0, 0, 0)
-              //         ],
-              //         begin: Alignment.bottomCenter,
-              //         end: Alignment.topCenter,
-              //       ),
-              //     ),
-              //     padding: EdgeInsets.symmetric(
-              //         vertical: 10.0, horizontal: 20.0),
-              //     child: Text(
-              //       'No. ${imgList.indexOf(item)} image',
-              //       style: TextStyle(
-              //         color: Colors.white,
-              //         fontSize: 20.0,
-              //         fontWeight: FontWeight.bold,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-            ],
-          )),
-    )).toList();
+  margin: const EdgeInsets.all(5.0),
+  child: ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+      child: Stack(
+        children: <Widget>[
+          Image.asset(item, fit: BoxFit.cover, width: 1000.0),
+          // Positioned(
+          //   bottom: 0.0,
+          //   left: 0.0,
+          //   right: 0.0,
+          //   child: Container(
+          //     decoration: BoxDecoration(
+          //       gradient: LinearGradient(
+          //         colors: [
+          //           Color.fromARGB(200, 0, 0, 0),
+          //           Color.fromARGB(0, 0, 0, 0)
+          //         ],
+          //         begin: Alignment.bottomCenter,
+          //         end: Alignment.topCenter,
+          //       ),
+          //     ),
+          //     padding: EdgeInsets.symmetric(
+          //         vertical: 10.0, horizontal: 20.0),
+          //     child: Text(
+          //       'No. ${imgList.indexOf(item)} image',
+          //       style: TextStyle(
+          //         color: Colors.white,
+          //         fontSize: 20.0,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+        ],
+      )),
+)).toList();
